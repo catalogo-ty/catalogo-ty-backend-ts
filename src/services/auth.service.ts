742cef4140527f4f095a708ef920358918372502
Williams Caballero Araya <williams.caballeroa@gmail.com>
2022-12-24 21:03:10 -0300

    Modificar servicios, controlador y agregar validaciones

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,46 +1,26 @@
-import { Response } from "express"
 import { createJwt } from "../helpers/create-jwt";
 import { comparePassword } from "../helpers/encrypt";
 import { IUser } from "../interfaces/user.interface";
-
 import User from "../models/user"
 
-export const verificarUsuario = async (email: string, password: string, res: Response): Promise<IUser | undefined> => {
-
-    try {
-        // Verificar que el email esta en la bd
-        const user = await User.findOne({email});
-        if (!user) {
-            res.status(400).json({
-                msg: `Correo y/o contraseña incorrectos`
-            })
-        }
-        // Verificar estatus del usuario
-        if (user?.status === false) {
-            res.status(400).json({
-                msg: `Correo y/o contraseña incorrectos`
-            })
-        }
-        // Comparar password del body con el password encriptado del usuario
-        const validPassword = comparePassword(password, user!.password)
-        if (!validPassword) {
-            res.status(400).json({
-                msg: `Correo y/o contraseña incorrectos`
-            })
-        }
-        
-
-        // Retornar usuario
-        return user!
-
-    } catch (error) {
-        console.log({error});
-    }
-
+export const verificarUsuario = async (email: string): Promise<IUser | null> => {
+    // Verificar que el email esta en la bd
+    const user = await User.findOne({ email }, {
+        _id:1,
+        name:1,
+        email:1,
+        password:1
+    })
+    return user
 }
 
-export const generarJWT = async (id:string): Promise<string> => {
-    
+export const verificarPassword = ( password: string, userPassword:string): boolean => {
+    const validPassword = comparePassword(password, userPassword);
+    return validPassword;
+}
+
+export const generarJWT = async (id: string): Promise<string> => {
+
     const jwt = await createJwt(id)
     return jwt
 
